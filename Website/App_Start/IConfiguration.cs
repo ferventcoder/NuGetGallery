93d3c1d4ce90c9f719adf1c328d2d2a5d8458acb
Rob Reynolds <ferventcoder@gmail.com>
2011-09-27 15:59:35 -0500

    implementing S3 integration

--- Website/App_Start/IConfiguration.cs
+++ Website/App_Start/IConfiguration.cs
@@ -5,5 +5,9 @@ namespace NuGetGallery {
         bool ConfirmEmailAddresses { get; }
         string PackageFileDirectory { get; }
         MailAddress GalleryOwnerEmailAddress { get; }
+        bool UseAwsSimpleStorageService { get; }
+        string S3Bucket { get; }
+        string S3AccessKey { get; }
+        string S3SecretKey { get; }
     }
 }
